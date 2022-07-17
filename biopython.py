b8ba3eb769ae04e02ed2c24af38607587a8ce605
Bladimir Pardo <bladimirpardomolina@gmail.com>
2022-07-17 16:33:04 -0400

    avance

---
+++ biopython.py
@@ -0,0 +1,50 @@
+from Bio.Seq import Seq
+from Bio import Entrez
+from Bio import SeqIO
+from Bio.Blast import NCBIWWW
+from Bio.Blast import NCBIXML
+import sys
+
+# Bladimir Pardo
+# Daniel Diaz
+# Nicol Huaiquil
+# Felipe Lagos
+
+def main():
+    
+    print(f'Llamada con {len(sys.argv)} argumentos: \t{sys.argv}')
+    #Validacion de Argumentos
+    if len(sys.argv) != 2:
+        print(f'USO: {sys.argv[0]} <Hebra>')
+        sys.exit()
+
+    seq = sys.argv[1]
+    
+    #Blasteo
+    print('BLASTeando secuencia...')
+    result_handle = NCBIWWW.qblast("blastn", "nt", seq)
+    with open("resultado_blast.xml", "w") as out_handle:
+        out_handle.write(result_handle.read())
+    result_handle.close()
+    print('archivo almacenado')
+
+    resultados = open("resultado_blast.xml")
+    blast_record = NCBIXML.read(resultados)
+
+    for alineamiento in blast_record.alignments:  # alignments se refiere a los hits
+        for hsp in alineamiento.hsps: # recore HSPs
+            hit_seq = (alineamiento.title.split('|'))[1]
+            break
+        break
+    resultados.close()
+
+    #Descarga de Datos
+    print('Obteniendo información del gen...')   
+    with Entrez.efetch(db="nucleotide", rettype="gb", retmode="text", id=hit_seq) as handle:
+        seq_record = SeqIO.read(handle, "gb")
+        print(f'Especie: {seq_record.annotations["organism"]}')
+        print(f'Gen identificado: {seq_record.name} ')
+        print(f'Ubicación: {seq_record.features[0].location}')
+        print(f'Exones en el gen: {len(seq_record.features)}')
+
+main()
